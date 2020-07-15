52c7b73e71f78489327a3a794418bce0120a4f3b
Alexander Mikhailenko <molotok539@gmail.com>
2020-07-15 14:30:16 +0900

    Add hp bar for Enemy.

--- projectDirectory/Scens/Charecters/Enemy.cs
+++ projectDirectory/Scens/Charecters/Enemy.cs
@@ -5,4 +5,17 @@ using Static = projectDirectory.Static;
 public class Enemy : Character
 {
 
+    private TextureProgress _hpBar;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _hpBar = GetNode<TextureProgress>("HpBar");
+    }
+
+    private void _OnHpValueUpdate(int max, int current)
+    {
+        _hpBar.MaxValue = max;
+        _hpBar.Value = current;
+    }
 }
