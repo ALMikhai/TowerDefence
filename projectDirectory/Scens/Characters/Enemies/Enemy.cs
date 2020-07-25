d8b292c4306050b841dec977ec33993bc815d88c
Alexander Mikhailenko <molotok539@gmail.com>
2020-07-25 18:45:26 +0900

    Add depencing enemies stats by game level.

--- projectDirectory/Scens/Characters/Enemies/Enemy.cs
+++ projectDirectory/Scens/Characters/Enemies/Enemy.cs
@@ -6,13 +6,25 @@ public class Enemy : Character
 {
     [Export]
     public int Cost = 25;
+    [Export]
+    public int StartDamage = 10;
+    [Export]
+    public int StartSpeed = 50;
+    [Export]
+    public int StartHp = 100;
 
     private TextureProgress _hpBar;
+    protected Global _global;
 
     public override void _Ready()
     {
         base._Ready();
         _hpBar = GetNode<TextureProgress>("HpBar");
+        _global = GetTree().Root.GetNode<Global>("Global");
+
+        _damage = StartDamage + (int)Math.Sqrt(_global.GetLevel() * 5);
+        Speed = StartSpeed + (int)Math.Sqrt(_global.GetLevel() * 500);
+        _healthNode.SetHealth(StartHp + (int)Math.Sqrt(_global.GetLevel() * 150));
     }
     public int GetCost() => Cost;
 
