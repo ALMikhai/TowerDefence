050018bc12b25f4a142b9abdcfb591898c57d978
Alexander Mikhailenko <molotok539@gmail.com>
2020-08-02 13:22:38 +0900

    Add shop card textures.

--- projectDirectory/Scens/Characters/Defenders/Defender.cs
+++ projectDirectory/Scens/Characters/Defenders/Defender.cs
@@ -4,26 +4,26 @@ using Static = projectDirectory.Static;
 
 public class Defender : Character
 {
-    [Export]
-    public PackedScene Shell;
+	[Export]
+	public PackedScene Shell;
 
-    private DefendersData _defendersData;
+	private DefendersData _defendersData;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _defendersData = GetTree().Root.GetNode<DefendersData>("DefendersData");
+	public override void _Ready()
+	{
+		base._Ready();
+		_defendersData = GetTree().Root.GetNode<DefendersData>("DefendersData");
 
-        Damage = (int)(StartDamage + Math.Sqrt(350 * _defendersData.GetDefenderLevel(Type)));
-        ReloadTime = StartReloadTime * (1 / (_defendersData.GetDefenderLevel(Type) * 0.5f));
-        _reloadTimer.WaitTime = ReloadTime;
-    }
+		Damage = (int)(StartDamage + Math.Sqrt(350 * _defendersData.GetDefenderLevel(Type)));
+		ReloadTime = StartReloadTime * (1 / (_defendersData.GetDefenderLevel(Type) * 0.5f));
+		_reloadTimer.WaitTime = ReloadTime;
+	}
 
-    public override void Attack()
-    {
-        var shell = (Shell)Shell.Instance();
-        AddChild(shell);
-        shell.SetDamage(Damage);
-        shell.SetDestination(_target.GlobalPosition);
-    }
+	public override void Attack()
+	{
+		var shell = (Shell)Shell.Instance();
+		AddChild(shell);
+		shell.SetDamage(Damage);
+		shell.SetDestination(_target.GlobalPosition);
+	}
 }
--- projectDirectory/Scens/MainMenu/Store/DefenderCell.cs
+++ projectDirectory/Scens/MainMenu/Store/DefenderCell.cs
@@ -3,42 +3,42 @@ using System;
 
 public class DefenderCell : Control
 {
-    [Export]
-    public ObjectCreator.Objects Type;
+	[Export]
+	public ObjectCreator.Objects Type;
 
-    private Defender _defenderNode;
-    private Position2D _defenderPosition;
-    private Label _damage;
-    private Label _level;
-    private Button _levelUpButton;
-    private DefendersData _defendersData;
+	private Defender _defenderNode;
+	private Position2D _defenderPosition;
+	private Label _dps;
+	private Label _level;
+	private Label _levelUpCostLabel;
+	private DefendersData _defendersData;
 
-    public override void _Ready()
-    {
-        _defenderPosition = GetNode<Position2D>("DefenderPosition");
-        _damage = GetNode<Label>("Damage/Value");
-        _level = GetNode<Label>("Level/Value");
-        _levelUpButton = GetNode<Button>("LevelUp");
-        _defendersData = GetTree().Root.GetNode<DefendersData>("DefendersData");
-        _defenderNode = (Defender)ObjectCreator.Create(Type);
+	public override void _Ready()
+	{
+		_defenderPosition = GetNode<Position2D>("DefenderPosition");
+		_dps = GetNode<Label>("Dps/Value");
+		_level = GetNode<Label>("Level");
+		_levelUpCostLabel = GetNode<Label>("LevelUp/Cost");
+		_defendersData = GetTree().Root.GetNode<DefendersData>("DefendersData");
+		_defenderNode = (Defender)ObjectCreator.Create(Type);
 
-        AddChild(_defenderNode);
-        _defenderNode.Position = _defenderPosition.Position;
+		AddChild(_defenderNode);
+		_defenderNode.Position = _defenderPosition.Position;
 
-        UpdateView();
-    }
+		UpdateView();
+	}
 
-    private void UpdateView()
-    {
-        _defenderNode._Ready();
-        _level.Text = _defendersData.GetDefenderLevel(Type).ToString();
-        _damage.Text = ((int)(_defenderNode.Damage / _defenderNode.ReloadTime)).ToString();
-        _levelUpButton.Text = _defendersData.GetNextLevelCost(Type).ToString();
-    }
+	private void UpdateView()
+	{
+		_defenderNode._Ready();
+		_level.Text = _defendersData.GetDefenderLevel(Type).ToString();
+		_dps.Text = ((int)(_defenderNode.Damage / _defenderNode.ReloadTime)).ToString();
+		_levelUpCostLabel.Text = _defendersData.GetNextLevelCost(Type).ToString();
+	}
 
-    private void _OnLevelUpPressed()
-    {
-        _defendersData.TryBuyLevel(Type);
-        UpdateView();
-    }
+	private void _OnLevelUpPressed()
+	{
+		_defendersData.TryBuyLevel(Type);
+		UpdateView();
+	}
 }
