79878c259e507d1cf961a444f580bbc1d3b33d0c
Alexander Mikhailenko <molotok539@gmail.com>
2020-08-03 12:12:30 +0900

    Fix coin flying bug.

--- projectDirectory/Scens/Characteristics/Money.cs
+++ projectDirectory/Scens/Characteristics/Money.cs
@@ -3,19 +3,15 @@ using System;
 
 public class Money : Node2D
 {
+	private Sprite _sprite;
 	private MoneyNode _moneyNode;
 	private bool _animationFinished = false;
 	private int _speed = 700;
 	private int _cost = 0;
 
-	public void SetMoneyNode(MoneyNode node)
+	public override void _Ready()
 	{
-		_moneyNode = node;
-	}
-
-	public void SetCost(int cost)
-	{
-		_cost = cost;
+		_sprite = GetNode<Sprite>("Sprite");
 	}
 
 	public override void _PhysicsProcess(float delta)
@@ -23,8 +19,8 @@ public class Money : Node2D
 		if (_moneyNode == null || !_animationFinished)
 			return;
 
-		GlobalPosition += (_moneyNode.GlobalPosition - GlobalPosition).Normalized() * delta * _speed;
-		if (GlobalPosition.DistanceTo(_moneyNode.GlobalPosition) <= 10)
+		_sprite.GlobalPosition += (_moneyNode.GlobalPosition - _sprite.GlobalPosition).Normalized() * delta * _speed;
+		if (_sprite.GlobalPosition.DistanceTo(_moneyNode.GlobalPosition) <= 10)
 		{
 			_moneyNode.Add(_cost);
 			_moneyNode.UpdateLabel();
@@ -35,5 +31,15 @@ public class Money : Node2D
 	private void _OnAnimationPlayerAnimationFinished(string animationName)
 	{
 		_animationFinished = true;
+	}
+	
+	public void SetMoneyNode(MoneyNode node)
+	{
+		_moneyNode = node;
+	}
+
+	public void SetCost(int cost)
+	{
+		_cost = cost;
 	}
 }
--- projectDirectory/Scens/Characteristics/MoneyNode.cs
+++ projectDirectory/Scens/Characteristics/MoneyNode.cs
@@ -3,59 +3,59 @@ using System;
 
 public class MoneyNode : Node2D
 {
-    [Signal]
-    public delegate void MoneyChange(int current);
+	[Signal]
+	public delegate void MoneyChange(int current);
 
-    private Label _valueLabel;
-    private int _money = 0;
-    private AudioStreamPlayer _goodTransactionSound;
-    private AudioStreamPlayer _badTransactionSound;
+	private Label _valueLabel;
+	private int _money = 0;
+	private AudioStreamPlayer _goodTransactionSound;
+	private AudioStreamPlayer _badTransactionSound;
 
-    public override void _Ready()
-    {
-        _goodTransactionSound = GetNode<AudioStreamPlayer>("GoodTransaction");
-        _badTransactionSound = GetNode<AudioStreamPlayer>("BadTransaction");
-    }
+	public override void _Ready()
+	{
+		_goodTransactionSound = GetNode<AudioStreamPlayer>("GoodTransaction");
+		_badTransactionSound = GetNode<AudioStreamPlayer>("BadTransaction");
+	}
 
-    public int Get() => _money;
+	public int Get() => _money;
 
-    public void Add(int money)
-    {
-        _money += money;
-        UpdateLabel();
-    }
+	public void Add(int money)
+	{
+		_money += money;
+		UpdateLabel();
+	}
 
-    public void ViewAdd(int money, Vector2 position)
-    {
-        var moneyView = (Money)GD.Load<PackedScene>("res://Scens/Characteristics/Money.tscn").Instance();
-        AddChild(moneyView);
-        moneyView.GlobalPosition = position;
-        moneyView.SetMoneyNode(this);
-        moneyView.SetCost(money);
-    }
+	public void ViewAdd(int money, Vector2 position)
+	{
+		var moneyView = (Money)GD.Load<PackedScene>("res://Scens/Characteristics/Money.tscn").Instance();
+		AddChild(moneyView);
+		moneyView.GlobalPosition = position;
+		moneyView.SetMoneyNode(this);
+		moneyView.SetCost(money);
+	}
 
-    public bool TrySpend(int money)
-    {
-        if (money <= _money)
-        {
-            _money -= money;
-            _goodTransactionSound.Play();
-            UpdateLabel();
-            return true;
-        }
-        else
-        {
-            _badTransactionSound.Play();
-            return false;
-        }
-    }
+	public bool TrySpend(int money)
+	{
+		if (money <= _money)
+		{
+			_money -= money;
+			_goodTransactionSound.Play();
+			UpdateLabel();
+			return true;
+		}
+		else
+		{
+			_badTransactionSound.Play();
+			return false;
+		}
+	}
 
-    public void UpdateLabel()
-    {
-        if (_valueLabel == null)
-            _valueLabel = GetNode<Label>("Coin/Value");
+	public void UpdateLabel()
+	{
+		if (_valueLabel == null)
+			_valueLabel = GetNode<Label>("Coin/Value");
 
-        _valueLabel.Text = _money.ToString();
-        EmitSignal(nameof(MoneyChange));
-    }
+		_valueLabel.Text = _money.ToString();
+		EmitSignal(nameof(MoneyChange));
+	}
 }
